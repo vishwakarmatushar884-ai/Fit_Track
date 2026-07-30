170b4ff6c223a31f932357205354f1a0a239c7b2
TUSHAR VISHWAKARMA <vishwakarmatushar884@gmail.com>
2026-07-31 02:13:37 +0530

    Downgraded prisma

--- server/node_modules/.prisma/client/edge.js
+++ server/node_modules/.prisma/client/edge.js
@@ -323,6 +323,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- server/node_modules/.prisma/client/index.js
+++ server/node_modules/.prisma/client/index.js
@@ -324,6 +324,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
